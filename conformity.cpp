e044f74fa4fcc3cac41f83d679700375b5978e4e
andrewome <andrewong3012@gmail.com>
2019-02-08 22:52:59 +0800

    Create conformity.cpp

---
+++ conformity.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int numStudents;
+    int i, j;
+    unordered_map<string, int> m;
+    
+    cin >> numStudents;
+    for(i=0;i<numStudents;i++) {
+        vector<int> v;
+        for(j=0;j<5;j++) {
+            int moduleCode;
+            cin >> moduleCode;
+            v.push_back(moduleCode);
+        }
+        string output = "";
+        sort(v.begin(), v.end());
+        for(int j : v) {
+            output += to_string(j);
+        }
+        if(m.find(output) == m.end()) {
+            m.insert({output, 1});
+        } else {
+            m[output]++;
+        }
+    }
+    int max = 0;
+    for(pair<string, int> p : m) {
+        if(p.second > max) {
+            max = p.second;
+        }
+    }
+    int total = 0;
+    for(pair<string, int> p : m) {
+        if(p.second == max) {
+            total += max;
+        }
+    }
+    
+    cout << total << endl;
+}
